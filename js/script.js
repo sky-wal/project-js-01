93af14191b4a72445a39dff2c9d6f61fae0a3647
vknyazev <skywal@skywal.ru>
2022-09-29 21:05:20 +0700

    Lesson 012

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-"use sctrict";
+//'use strict';
 
-let number = 4.6;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(-4/0);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?"), 
+      b = prompt("На сколько оцените его?"),
+      c = prompt("Один из последних просмотренных фильмов?"), 
+      d = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
