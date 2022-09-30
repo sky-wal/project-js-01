1d1e086ec891f4dd1d990a80d57f66e404ec96c4
vknyazev <skywal@skywal.ru>
2022-09-30 15:03:22 +0700

    Lesson 015

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//'use strict';
+'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
@@ -10,12 +10,25 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?"), 
-      b = prompt("На сколько оцените его?"),
-      c = prompt("Один из последних просмотренных фильмов?"), 
-      d = prompt("На сколько оцените его?");
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?"), 
+          b = prompt("На сколько оцените его?");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('OK');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 console.log(personalMovieDB);
